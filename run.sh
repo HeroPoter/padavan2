f9e2ef0a3d7f2e4247292738f1c539becfb952fe
HeroPoter <68175960+HeroPoter@users.noreply.github.com>
2023-03-05 23:43:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ for ((i=1;i<=3;i++))     #此处也可以写  for i in {1..9}
   do
   a=$1+10
   b=$1+11
+  echo "$a,$b"
         sudo sed -i "29c remote_port = $a" /opt/modify/squashfs-root/sbin/frp_script.sh
         sudo sed -i "36c remote_port = $b" /opt/modify/squashfs-root/sbin/frp_script.sh
         sudo sed -i "4c open ww1.rank100.top 21" /opt/modify/ftpup.sh
@@ -11,4 +12,5 @@ for ((i=1;i<=3;i++))     #此处也可以写  for i in {1..9}
         sudo cp -f newrom$a.trx /opt/modify/images/
         sudo cp -f /opt/modify/squashfs-root/sbin/frp_script.sh /opt/modify/images/
         sudo cp -f ftpup.sh /opt/modify/images/
+   echo "newrom$a.trx"
   done
