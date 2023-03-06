25f67009bbeb955f7607b4b018550b4c3c921132
HeroPoter <68175960+HeroPoter@users.noreply.github.com>
2023-03-06 15:28:57 +0800

    Update runfor.sh

--- runfor.sh
+++ runfor.sh
@@ -4,10 +4,5 @@ for i in {1..3}
   a=`expr $1 + $i \* 10 - 10`
   b=`expr $1 + $i \* 10 - 9`
   echo "$a,$b"
-  sudo sed -i "29c remote_port = $a" /opt/modify/squashfs-root/sbin/frp_script.sh
-  sudo sed -i "36c remote_port = $b" /opt/modify/squashfs-root/sbin/frp_script.sh
-  sh modify.sh c newrom$a.trx
-  sudo cp -f newrom$a.trx /opt/modify/images/
-  sudo cp -f /opt/modify/squashfs-root/sbin/frp_script.sh /opt/modify/images/frp_script$a.sh
   echo "newrom$a.trx"
   done
