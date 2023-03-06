b754dc6b1bb5ea1d579bca2818b283a15a40a635
HeroPoter <68175960+HeroPoter@users.noreply.github.com>
2023-03-06 09:33:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in {1..3}     #此处也可以写  for i in {1..9}
   do
-  a=$(expr $1 + $i "*" 10 - 10)
-  b=$(expr $1 + $i "*" 10 - 9)
+  a=$(expr $1 +  10)
+  b=$(expr $1 +  11)
   echo "$a,$b"
   echo "newrom$a.trx"
   done
