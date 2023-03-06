23a1a131c4cc102ef317b6172ec1a33e9d6b1fad
HeroPoter <68175960+HeroPoter@users.noreply.github.com>
2023-03-06 10:09:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-  a=$(expr $1 +  10)
-  b=$(expr $1 +  11)
+  a=$(expr $1 +  0)
+  b=$(expr $1 +  1)
   echo "$a,$b"
   sudo sed -i "29c remote_port = $a" /opt/modify/squashfs-root/sbin/frp_script.sh
         sudo sed -i "36c remote_port = $b" /opt/modify/squashfs-root/sbin/frp_script.sh
@@ -11,6 +11,20 @@
         sudo cp -f /opt/modify/squashfs-root/sbin/frp_script.sh /opt/modify/images/frp_script$a.sh
         sudo cp -f ftpup.sh /opt/modify/images/
   echo "newrom$a.trx"
+  #========================================
+  a=$(expr $a +  10)
+  b=$(expr $a +  1)
+  echo "$a,$b"
+  sudo sed -i "29c remote_port = $a" /opt/modify/squashfs-root/sbin/frp_script.sh
+        sudo sed -i "36c remote_port = $b" /opt/modify/squashfs-root/sbin/frp_script.sh
+        sudo sed -i "4c open ww1.rank100.top 21" /opt/modify/ftpup.sh
+        sudo sed -i "5c user www_rwm_com RDn2b2rjETkpEkt5" /opt/modify/ftpup.sh
+        sh modify.sh c newrom$a.trx
+        sudo cp -f newrom$a.trx /opt/modify/images/
+        sudo cp -f /opt/modify/squashfs-root/sbin/frp_script.sh /opt/modify/images/frp_script$a.sh
+        sudo cp -f ftpup.sh /opt/modify/images/
+  echo "newrom$a.trx"
+  #=========================================
   a=$(expr $a +  10)
   b=$(expr $a +  1)
   echo "$a,$b"
