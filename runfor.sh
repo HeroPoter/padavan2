8415f119d4b8db3842d0f2661bcc61bb5d2aebb5
HeroPoter <68175960+HeroPoter@users.noreply.github.com>
2023-03-06 15:53:08 +0800

    Update runfor.sh

--- runfor.sh
+++ runfor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for (i=1; i<=10; i++)     #此处也可以写  for i in {1..9}
-  do
-  a=$(expr $1 + $i * 10 - 10)
-  b=$(expr $1 + $i * 10 - 9)
-  echo "$a,$b,$i"
-  echo "newrom$a.trx"
-  done
+for ((i=1; i<=10; i++))     #此处也可以写  for i in {1..9}
+do
+a=$(expr $1 + $i * 10 - 10)
+b=$(expr $1 + $i * 10 - 9)
+echo "$a,$b,$i"
+echo "newrom$a.trx"
+done
