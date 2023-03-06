611114991bdf4447c90e409ad335d5b1772f346c
HeroPoter <68175960+HeroPoter@users.noreply.github.com>
2023-03-06 15:49:53 +0800

    Update runfor.sh

--- runfor.sh
+++ runfor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=1; i<=10; i++))     #此处也可以写  for i in {1..9}
+for (i=1; i<=10; i++)     #此处也可以写  for i in {1..9}
   do
   a=$(expr $1 + $i * 10 - 10)
   b=$(expr $1 + $i * 10 - 9)
